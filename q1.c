f1a51962d05174b203f3a2ca21864b1295481bf9
jordan <jordantangy@gmail.com>
2020-06-01 13:12:10 +0300

    fixing

--- q1.c
+++ q1.c
@@ -3,22 +3,23 @@
 #include <signal.h>
 #include <sys/types.h>
 
+int fd[2];
+int returnval;
 
-void parent_handler(int num){
- 
+void parent_handler(){
+    
+    
+
 
 }
-void child_handler(int num){
-
-  
-
+void child_handler(){
+    int x = 0;
+    write(fd[1],&x,sizeof(int));
+    signal(SIGUSR1,parent_handler);
+    close(fd[1]);
 }
 
 int main(){
-    
-
-    int fd[2];
-    int returnval;
     returnval = pipe(fd);
     if(returnval == -1 ){
         printf("An error occured while opening the pipe\n");
@@ -28,14 +29,10 @@ int main(){
     pid_t pid = fork();
     int parend_id = getppid();
     if(pid == 0){
-        int x = 0;
-        write(fd[1],&x,sizeof(int));
-        signal(SIGUSR1,parend_id);
-        close(fd[1]);
-
+        child_handler();
     }
-   
-   
-
+    else{
+        parent_handler();
+    }
 }
 
